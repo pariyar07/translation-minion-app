a3f6e480d2e75de8db0edd3ddf419c56343cf25c
Satyam Pariyar <pariyarsatyam1@gmail.com>
2021-09-17 09:58:10 +0530

    minor changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
@@ -14,10 +13,8 @@ function errorHandler(error) {
     alert("something wrong with server! try again after some time")
 }
 
-
 function clickHandler() {
     var inputText = txtInput.value;
-
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
